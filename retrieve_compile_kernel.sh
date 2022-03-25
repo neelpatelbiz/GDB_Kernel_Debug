fbaaacb01e40d13beaaaf1485c0b48c817c02d92
Neel Patel <n869p538@bench.ittc.ku.edu>
2022-03-25 11:38:23 -0500

    initial

--- retrieve_compile_kernel.sh
+++ retrieve_compile_kernel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.16.17.tar.xz
-#tar xvof linux-5.16.17.tar.xz
-#cd linux-5.16.17.tar.xz
-#make defconfig
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.16.17.tar.xz
+tar xvof linux-5.16.17.tar.xz
+cd linux-5.16.17
+make defconfig
 cpus=$(($(nproc) / 2))
-echo $cpus
-#sudo make -j $(( $cpus ))
+echo "compiling kernel using $cpus cpus ..."
+sudo make -j $cpus
+sudo make modules_install -j $cpus
