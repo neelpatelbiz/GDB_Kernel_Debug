84fcc511c18281310de37aa0015efab485bddd5b
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-04-05 20:34:10 -0500

    nmp copy function

--- Builds/system-image-x86_64/run-emulator.sh
+++ Builds/system-image-x86_64/run-emulator.sh
@@ -3,7 +3,7 @@
 # Boot the emulated system to a shell prompt.
 #QEMU_EXTRA="-s -S -netdev user,id=n1 -device virtio-net-pci,netdev=n1 "
 #QEMU_EXTRA="-s -S -netdev user,id=n1 -device e1000,netdev=n1"
-QEMU_EXTRA="-s -nic user,ipv6=off,model=e1000,mac=52:54:98:76:54:32,hostfwd=tcp:127.0.0.1:4433-:4433"
+QEMU_EXTRA="-s -S -nic user,ipv6=off,model=e1000,mac=52:54:98:76:54:32,hostfwd=tcp:127.0.0.1:4433-:4433"
 #QEMU_EXTRA="-s -S -netdev user,id=n0,hostfwd=192.168.1.2:4433-10.0.2.15:4422"
 KERNEL_EXTRA="nokaslr"
 
--- scripts/retrieve_compile_kernel.sh
+++ scripts/retrieve_compile_kernel.sh
@@ -3,11 +3,12 @@ source scripts/settings.sh
 
 cd ${BUILD_DIR}
 [[ ! -f "${Linux_Version}.tar.xz" ]] && wget https://cdn.kernel.org/pub/linux/kernel/v5.x/${Linux_Version}.tar.xz --no-check-certificate
-if [[ ! -d "${Lin_Path}" ]]; then 
+if [[ ! -d "${Lin_Path}/arch/x86_64/boot/bzImage" ]]; then 
 	tar xvof ${Linux_Version}.tar.xz
 	cd ${Linux_Version}
 	make defconfig
 	echo "CONFIG_DEBUG_INFO=y" >> .config
+	echo "CONFIG_GDB_SCRIPTS=y" >> .config
 	sudo make -j $cpus
 	sudo make modules_install -j $cpus
 fi
